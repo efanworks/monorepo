47f9f15e2d010f83b4ddd91a6b3f8e7aef6bf192
claytonzhuo <eafanzhuo@foxmail.com>
2025-12-09 17:32:24 +0800

    remote依赖depapp

--- packages/remote-app/lib/remote-app.js
+++ packages/remote-app/lib/remote-app.js
@@ -1,7 +1,11 @@
-'use strict';
+"use strict";
+
+const depApp = require("@efanworks/dep-app");
+
+console.log(depApp());
 
 module.exports = remoteApp;
 
 function remoteApp() {
-  return 'Hello from remoteApp';
+  return "Hello from remoteApp";
 }
